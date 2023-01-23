888043f24c9c8d28f1fb6fd6dee31e32144b01be
jose cabello <cabello.ranea.jose@claretsevilla.org>
2023-01-23 12:30:53 +0100

    Test to push.

--- com.jose.git.first/src/com/jose/git/first/GitTest.java
+++ com.jose.git.first/src/com/jose/git/first/GitTest.java
@@ -3,5 +3,6 @@ package com.jose.git.first;
 public class GitTest {
 	  public static void main(String[] args) {
 	    System.out.println("Git is cool");
+	    System.out.println("Git is fun");
 	  }
 }
